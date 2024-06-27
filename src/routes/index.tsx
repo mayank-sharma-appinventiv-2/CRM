7c2b1f3d954fe52ff969d59b9d71c4b18fb9508a
mayank-sharma-appinventiv-2 <mayank.sharma2@appinventiv.com>
2024-06-27 16:29:28 +0530

    working on routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LazyLoader from "../utils/routesContainer";
+import { lazy } from "react";
+import PrivateRoute from "./privateRoute";
+import PublicRoute from "./publicRoute";
+
+const Login = LazyLoader(
+    lazy(() => import("../pages/Login"))
+  );
+
+  const Dashboard = LazyLoader(
+    lazy(() => import("../pages/Dashboard"))
+  );
 
 const RoutesManager = () => {
+    
     return (<BrowserRouter>
         <Routes>
             <Route
                 path={"/"}
                 element={
-                    <></>
+                    <PrivateRoute>
+                      <Dashboard/>
+                    </PrivateRoute>
                 }
             ></Route>
             <Route
                 path={"/login"}
                 element={
-                    <></>
+                    <PublicRoute>
+                    <Login/>
+                    </PublicRoute>
                 }
             ></Route>
         </Routes>
